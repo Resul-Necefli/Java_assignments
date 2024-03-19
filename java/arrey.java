1d3361d35f93a3467108fcc8361973294cdff914
Resul-Necefli <necefliresul29@gmail.com>
2024-03-19 19:57:48 +0400

    sorted Unsorted

--- java/arrey.java
+++ java/arrey.java
@@ -1,2 +1,26 @@
+
+
+
 public class arrey {
+    public static void main(String[] args) {
+
+    int[] result = { 1, 10, 3, 4 };
+    int i = 0;while(i<result.length-1)
+    {
+        //              int b =0;
+        int s = result.length - 1;
+
+        if (result[s] > result[i]) {
+
+            System.out.println("Sorted");
+
+        } else {
+            System.out.println("Unsorted");
+        }
+
+        i++;
+    }
+
+}
+
 }
