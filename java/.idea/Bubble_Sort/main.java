370225e6f4650acdb21001bf9babf98c0b59c343
Resul-Necefli <necefliresul29@gmail.com>
2024-03-25 17:22:54 +0400

    buble sort it was done 2

--- java/.idea/Bubble_Sort/main.java
+++ java/.idea/Bubble_Sort/main.java
@@ -11,7 +11,7 @@ public class main {
         for (int i = 0; i < size - 1; i++) {
 
 
-            for (int j = 0; j < size - 1; j++) {
+            for (int j = 0; j < size -i - 1; j++) {
 
                 if (arr[j] > arr[j + 1]) {
 
